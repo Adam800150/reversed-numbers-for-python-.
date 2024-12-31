7d434093ea1cabac4819b5d3c96c5b2205ce447e
Adam800150 <andomyshehab2013@gmail.com>
2024-12-31 17:40:51 +0100

    Add files via upload

---
+++ numb3.py
@@ -0,0 +1,4 @@
+num = int(input("Enter a number: "))
+# Convert the number to a string, reverse the string, and convert it back to an integer
+vress = int(str(num)[::-1])
+print("Reversed number:", vress)
